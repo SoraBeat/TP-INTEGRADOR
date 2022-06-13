416e9fa6e61b3249444ee39033e8c5a3b8a7a7b7
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-06-12 23:02:17 -0300

    abm complejo web form

--- Proyecto Programacion/Vistas/ABMComplejo.aspx.cs
+++ Proyecto Programacion/Vistas/ABMComplejo.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace Vistas
 {
-    public partial class ABMComplejo : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+	public partial class ABMComplejo : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        }
-    }
+		}
+	}
 }
