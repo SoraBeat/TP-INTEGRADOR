142a1ea5626b28eba60ca9e58731143c66b8f72c
GabrielBettiga <gustavo.bettiga@alumnos.frgp.utn.edu.ar>
2022-06-16 19:58:48 -0300

    Agregando Negocios part 2
    Asientos, Salas, Usuarios

---
+++ Proyecto Programacion/Negocios/NegocioAsientos.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAO;
+using Entidades;
+
+namespace Negocios
+{
+    class NegocioAsientos
+    {
+        public DataTable getTabla()
+        {
+            DaoAsientos dao = new DaoAsientos();
+            return dao.getTablaAsientos();
+        }
+        public DataTable getTablaPorID(int id)
+        {
+            DaoAsientos dao = new DaoAsientos();
+            return dao.getTablaAsientosPorID(id);
+        }
+        public DataTable getListaPeliculas()
+        {
+            DaoAsientos dao = new DaoAsientos();
+            return dao.getListaAsientos();
+        }
+
+        public Asientos get(int id)
+        {
+            DaoAsientos dao = new DaoAsientos();
+            Asientos Asi = new Asientos();
+            Asi.IdAsiento = id;
+            return dao.getAsientos(Asi);
+        }
+        public DataTable CrearTablaSession()
+        {
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("ID_Asiento_A", typeof(string));
+            tabla.Columns.Add("ID_Sala_A", typeof(string));
+            tabla.Columns.Add("ID_Complejo_A", typeof(string));
+
+            return tabla;
+        }
+        public bool EliminarPelicula(int id)
+        {
+            DaoAsientos dao = new DaoAsientos();
+            Asientos Asi = new Asientos();
+            Asi.IdAsientos= id;
+            int op = dao.EliminarAsiento(Asi);
+            if (op == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool AgregarPelicula(string ID_Asiento_A, string ID_Sala_A, string ID_Complejo_A)
+        {
+            int cantFilas = 0;
+            DaoAsientos daoAsi = new DaoAsientos();
+            Asientos Asi = new Asientos();
+            Asi.ID_Asiento = ID_Asiento_A;
+            Asi.ID_Sala = ID_Sala_A;
+            Asi.ID_Complejo = ID_Complejo_A;
+
+            if (daoAsi.ExisteAsiento(Asi) == false)
+            {
+                cantFilas = daoAsi.AgregarAsiento(Asi);
+            }
+            if (cantFilas == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Proyecto Programacion/Negocios/NegocioSalas.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAO;
+using Entidades;
+
+namespace Negocios
+{
+    public class NegocioSalas
+    {
+        public DataTable getTabla()
+        {
+            DaoSalas dao = new DaoSalas();
+            return dao.getTablaSalas();
+        }
+        public DataTable getTablaPorID(int id)
+        {
+            DaoSalas dao = new DaoSalas();
+            return dao.getTablaSalasPorID(id);
+        }
+        public DataTable getListaNegocios()
+        {
+            DaoSalas dao = new DaoSalas();
+            return dao.getListaSalas();
+        }
+
+        public Salas get(int id)
+        {
+            DaoSalas dao = new DaoSalas();
+            Salas Sal = new Salas();
+            Sal.IdSalas = id;
+            return dao.getSalas(Sal);
+        }
+        public DataTable CrearTablaSession()
+        {
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("ID_Sala_S", typeof(string));
+            tabla.Columns.Add("ID_Complejo_S", typeof(string));
+            tabla.Columns.Add("Total_Asientos_S", typeof(int));
+            tabla.Columns.Add("Estado_S", typeof(bool));
+
+            return tabla;
+        }
+        public bool EliminarPelicula(int id)
+        {
+            DaoSalas dao = new DaoSalas();
+            Salas Sal = new Salas();
+            Sal.IdSala = id;
+            int op = dao.EliminarSala(Sal);
+            if (op == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool AgregarPelicula(string ID_Sala_S, string ID_Complejo_S, int Total_Asientos_S, bool Estado_S)
+        {
+            int cantFilas = 0;
+            DaoSalas daoSal = new DaoSalas();
+            Salas Sal = new Salas();
+            Sal.ID_Sala = ID_Sala_S;
+            Sal.ID_Complejo = ID_Complejo_S;
+            Sal.Total_Asientos = Total_Asientos_S;
+            Sal.Estado = Estado_S;
+
+            if (daoSal.ExisteSala(Sal) == false)
+            {
+                cantFilas = daoSal.AgregarSala(Sal);
+            }
+            if (cantFilas == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
