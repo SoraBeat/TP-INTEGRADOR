5518101171304009b9d0ed0d8d307c4168c81c7c
Agusrojas <roagustin00@gmail.com>
2022-06-18 01:16:23 -0300

    ABM Usuarios, Negocio Usuarios, Entidad Usuario
    Falta crear almacenamiento procesado

--- Proyecto Programacion/Negocios/NegocioUsuario.cs
+++ Proyecto Programacion/Negocios/NegocioUsuario.cs
@@ -11,82 +11,60 @@ namespace Negocios
 {
     public class NegocioUsuario
     {
-        //public DataTable getTabla()
-        //{
-        //    DaoUsuario dao = new DaoUsuario();
-        //    return dao.getTablaUsuario();
-        //}
-        //public DataTable getTablaPorID(int id)
-        //{
-        //    DaoUsuario dao = new DaoUsuario();
-        //    return dao.getTablaUsuariosPorID(id);
-        //}
-        //public DataTable getListaPeliculas()
-        //{
-        //    DaoUsuario dao = new DaoUsuario();
-        //    return dao.getListaUsuarios();
-        //}
+        public DataTable getTabla()
+        {
+            DAOUsuarios dao = new DAOUsuarios();
+            return dao.getTablaUsuarios();
+        }
+        public DataTable getTablaPorID(int id)
+        {
+            DAOUsuarios dao = new DAOUsuarios();
+            return dao.getTablaUsuariosPorID(id);
+        }
+        public DataTable getListaUsuarios()
+        {
+            DAOUsuarios dao = new DAOUsuarios();
+            return dao.getTablaUsuarios();
+        }
 
-        //public Usuarios get(int id)
-        //{
-        //    DaoUsuario dao = new DaoUsuario();
-        //    Usuarios Usu = new Usuarios();
-        //    Usu.IdUsuario = id;
-        //    return dao.getUsuario(Usu);
-        //}
-        //public DataTable CrearTablaSession()
-        //{
-        //    DataTable tabla = new DataTable();
-        //    tabla.Columns.Add("ID_Usuario_U", typeof(int));
-        //    tabla.Columns.Add("Nombre_U", typeof(string));
-        //    tabla.Columns.Add("Apellido_U", typeof(string));
-        //    tabla.Columns.Add("DNI_U", typeof(string));
-        //    tabla.Columns.Add("Telefono_U", typeof(string));
-        //    tabla.Columns.Add("Email_U", typeof(string));
-        //    tabla.Columns.Add("Contraseña_U", typeof(string));
-        //    tabla.Columns.Add("Tipo_Usuario_U", typeof(bool));
-        //    return tabla;
-        //}
-        //public bool EliminarUsuario(int id)
-        //{
-        //    DaoUsuario dao = new DaoUsuario();
-        //    Usuarios Usu = new Usuarios();
-        //    Usu.IdUsuario = id;
-        //    int op = dao.EliminarUsuario(Usu);
-        //    if (op == 1)
-        //    {
-        //        return true;
-        //    }
-        //    else
-        //    {
-        //        return false;
-        //    }
-        //}
-        //public bool AgregarPelicula(int ID_Usuario_U, string Nombre_U, string Apellido_U, string DNI_U, string Telefono_U, string Email_U, string Contraseña_U, bool Tipo_Usuario_U)
-        //{
-        //    int cantFilas = 0;
-        //    DaoUsuarios daoUsu = new DaoUsuarios();
-        //    Usuarios Usu = new Usuarios();
-        //    Usu.ID_Usuario = ID_Usuario_U;
-        //    Usu.Nombre = Nombre_U;
-        //    Usu.Apellido = Apellido_U;
-        //    Usu.DNI = DNI_U;
-        //    Usu.Telefono = Telefono_U;
-        //    Usu.Email = Email_U;
-        //    Usu.Contraseña = Contraseña_U;
-        //    Usu.Tipo_Usuario = Tipo_Usuario_U;
-        //    if (daoUsu.ExisteUsuario(Usu) == false)
-        //    {
-        //        cantFilas = daoUsu.AgregarUsuario(Usu);
-        //    }
-        //    if (cantFilas == 1)
-        //    {
-        //        return true;
-        //    }
-        //    else
-        //    {
-        //        return false;
-        //    }
-        //}
+        public Usuarios get(int id)
+        {
+            DAOUsuarios dao = new DAOUsuarios();
+            Usuarios Usu = new Usuarios();
+            Usu.IDUsuario = id;
+            return dao.getUsuario(Usu);
+        }
+        public bool EliminarUsuario(int id)
+        {
+            DAOUsuarios dao = new DAOUsuarios();
+            Usuarios Usu = new Usuarios();
+            Usu.IDUsuario = id;
+            int op = dao.EliminarUsuario(Usu);
+            if (op == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool AgregarUsuario(Usuarios usu)
+        {
+            int cantFilas = 0;
+            DAOUsuarios daoUsu = new DAOUsuarios();
+
+            cantFilas = daoUsu.AgregarUsuario(usu);
+
+            if (cantFilas == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
--- Proyecto Programacion/Vistas/ABMUsuario.aspx.cs
+++ Proyecto Programacion/Vistas/ABMUsuario.aspx.cs
@@ -4,14 +4,125 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Negocios;
+using Entidades;
+using System.Data;
+
 
 namespace Vistas
 {
     public partial class ABMUsuario : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+		NegocioUsuario negUsu = new NegocioUsuario();
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!IsPostBack)
+			{
+				CargarTablaSinFiltro();
+			}
+		}
+		private void CargarTablaSinFiltro()
+		{
+			DataTable tablaUsuarios = negUsu.getListaUsuarios();
+			gvUsuarios.DataSource = tablaUsuarios;
+			gvUsuarios.DataBind();
+		}
 
-        }
+        protected void gvUsuarios_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+			String ID_Usuario = ((Label)gvUsuarios.Rows[e.RowIndex].FindControl("LBL_IT_ID")).Text;
+			bool res = negUsu.EliminarUsuario(Convert.ToInt32(ID_Usuario));
+			if (res)
+			{
+				lblResultado.ForeColor = System.Drawing.Color.Green;
+				lblResultado.Text = "Se ha borrado correctamente";
+			}
+			else
+			{
+				lblResultado.ForeColor = System.Drawing.Color.Red;
+				lblResultado.Text = "ERROR al borrar";
+			}
+			CargarTablaSinFiltro();
+		}
+
+        protected void gvUsuarios_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+			gvUsuarios.EditIndex = e.NewEditIndex;
+			CargarTablaSinFiltro();
+		}
+
+        protected void gvUsuarios_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+			gvUsuarios.EditIndex = -1;
+			CargarTablaSinFiltro();
+		}
+
+        protected void gvUsuarios_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+			String ID = ((TextBox)gvUsuarios.Rows[e.RowIndex].FindControl("TXT_EDIT_ID")).Text;
+			String Nombre = ((TextBox)gvUsuarios.Rows[e.RowIndex].FindControl("TXT_EDIT_NOM")).Text;
+			String Apellido = ((TextBox)gvUsuarios.Rows[e.RowIndex].FindControl("TXT_EDIT_APE")).Text;
+			String Dni = ((TextBox)gvUsuarios.Rows[e.RowIndex].FindControl("TXT_EDIT_DNI")).Text;
+			String Telefono = ((TextBox)gvUsuarios.Rows[e.RowIndex].FindControl("TXT_EDT_TELEFONO")).Text;
+			String Email = ((TextBox)gvUsuarios.Rows[e.RowIndex].FindControl("TXT_EDIT_EMAIL")).Text;
+			String Contraseña = ((TextBox)gvUsuarios.Rows[e.RowIndex].FindControl("TXT_EDIT_CON")).Text;
+			String Tipo_Usuario = ((TextBox)gvUsuarios.Rows[e.RowIndex].FindControl("TXT_EDT_USU")).Text;
+
+
+			Usuarios usu = new Usuarios();
+			usu.IDUsuario = Convert.ToInt32(ID);
+			usu.NombreUsuario = Nombre;
+			usu.ApellidoUsuario = Apellido;
+			usu.DNIUsuario = Dni;
+			usu.TelefonoUsuario = Telefono;
+			usu.EmailUsuario = Email;
+			usu.ContraseñaUsuario = Contraseña;
+			usu.TipoUsuario = Convert.ToBoolean(Tipo_Usuario);
+
+			negUsu.AgregarUsuario(usu);
+			gvUsuarios.EditIndex = -1;
+			CargarTablaSinFiltro();
+
+		}
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+			if (IsValid)
+			{
+				int ID = Convert.ToInt32(txtID.Text);
+				String Nombre = txtNombre.Text;
+				String Apellido = txtApellido.Text;
+				String Dni = txtDni.Text;
+				String Telefono = txtTelefono.Text;
+				String Email = txtEmail.Text;
+				String Contraseña = txtContraseña.Text;
+				bool Tipo_Usuario = chkSuperUsu.Checked;
+
+				Usuarios usu = new Usuarios();
+				usu.IDUsuario = ID;
+				usu.NombreUsuario = Nombre;
+				usu.ApellidoUsuario = Apellido;
+				usu.DNIUsuario = Dni;
+				usu.TelefonoUsuario = Telefono;
+				usu.EmailUsuario = Email;
+				usu.ContraseñaUsuario = Contraseña;
+				usu.TipoUsuario = Tipo_Usuario;
+
+
+				bool res = negUsu.AgregarUsuario(usu);
+				if (res)
+				{
+					lblResultadoGuardar.ForeColor = System.Drawing.Color.Green;
+					lblResultadoGuardar.Text = "Se ha guardado con exito";
+				}
+				else
+				{
+					lblResultadoGuardar.ForeColor = System.Drawing.Color.Red;
+					lblResultadoGuardar.Text = "ERROR al guardar";
+				}
+				gvUsuarios.EditIndex = -1;
+				CargarTablaSinFiltro();
+			}
+		}
     }
 }
