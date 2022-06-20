ff5324d87bc4f90e2833bdd4ecb591d5d19bcc06
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-06-20 16:16:06 -0300

    Estilos a detalle de ventas 😎

--- Proyecto Programacion/Vistas/ABMDetalleVentas.aspx.designer.cs
+++ Proyecto Programacion/Vistas/ABMDetalleVentas.aspx.designer.cs
@@ -24,15 +24,6 @@ namespace Vistas
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Control lbCerrarSesion.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lbCerrarSesion;
-
-        /// <summary>
         /// Control ddlFiltro.
         /// </summary>
         /// <remarks>
@@ -78,13 +69,13 @@ namespace Vistas
         protected global::System.Web.UI.WebControls.GridView gvDetalleVentas;
 
         /// <summary>
-        /// Control Label6.
+        /// Control Label5.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label6;
+        protected global::System.Web.UI.WebControls.Label Label5;
 
         /// <summary>
         /// Control hlComplejos.
